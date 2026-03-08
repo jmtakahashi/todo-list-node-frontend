871674a47b20023392584a21bed0df187af19a4c
Jason Takahashi <jmtakahashi@gmail.com>
2026-03-08 16:38:42 -0700

    add token check for todoList route protection.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 import TodoList from '../features/todo/TodoList';
 import Header from './Header';
 import Home from './Home';
@@ -7,8 +7,12 @@ import LoginForm from '../features/auth/login/LoginForm';
 import RegisterForm from '../features/auth/register/RegisterForm';
 import Footer from './Footer';
 import Prefetch from '../features/auth/Prefetch';
+import { useSelector } from 'react-redux';
+import { getToken } from '../features/auth/authSlice';
 
 function App() {
+  const token = useSelector(getToken);
+
   return (
     <BrowserRouter>
       <div className='container'>
@@ -18,7 +22,7 @@ function App() {
             <Route path='/' element={ <Home /> }/>
             <Route path='/register' element={ <RegisterForm /> }/>
             <Route path='/login' element={ <LoginForm/> }/>
-            <Route path='/todoList' element={<Prefetch><TodoList /></Prefetch>}/>
+            <Route path='/todoList' element={token ? <Prefetch><TodoList /></Prefetch> : <Navigate to='/login' /> }/>
           </Routes>
         </main>
         <Footer />
