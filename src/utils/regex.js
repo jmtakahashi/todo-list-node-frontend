13c74fb30c9600f3c51836ddc6568754375efdbd
Jason Takahashi <jmtakahashi@gmail.com>
2026-03-09 03:04:41 -0700

    update regex for username check.

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,3 +1,3 @@
-export const USERNAME_REGEX = /^[A-z][A-z0-9-_]{3,24}$/;
+export const USERNAME_REGEX = /^[A-z0-9-_]{3,24}$/;
 export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const PASSWORD_REGEX =/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
